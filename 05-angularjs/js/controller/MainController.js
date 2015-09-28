54d4fe2eb26855fc11cc0acb2affc2d90efe4cd6
fabianfaaaal <fabian.lammoglia@gmail.com>
2015-09-28 15:04:57 -0300

    Problema con el arreglo

--- 05-angularjs/js/controller/MainController.js
+++ 05-angularjs/js/controller/MainController.js
@@ -6,7 +6,7 @@ app.controller('MainController', ['$scope', function($scope) {
     year: 1999, 
     about: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.", 
     img: 'http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX214_AL_.jpg',
-  }; 
+  };
 
 
 console.log("MainController Working");
