431103042db8c923fe8a94045828ee3d6c2812a4
fabianfaaaal <fabian.lammoglia@gmail.com>
2015-10-01 16:44:50 -0300

    Arreglado el route, funciona el view

--- 05-angularjs/js/controller/MainController.js
+++ 05-angularjs/js/controller/MainController.js
@@ -2,12 +2,14 @@ app.controller('AppCtrl', ['$scope', function($scope) {
   $scope.title = 'Aprendiendo AngularJS Movies';
   $scope.movies = [ 
   { 
+    id: 0,
     name: 'Matrix', 
     year: 1999, 
     about: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.", 
     img: 'http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX214_AL_.jpg'
   },
   {
+    id: 1,
   	name: 'Indiana Jones', 
     year: 1984, 
     about: 'After arriving in India, Indiana Jones is asked by a desperate village to find a mystical stone. He agrees, and stumbles upon a secret cult plotting a terrible plan in the catacombs of an ancient palace.', 
